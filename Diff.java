2df404fcdfeb8b90c9665f730175427453495e48
Gaurav Mule <gauravmule3134@gmail.com>
2024-03-10 20:11:13 +0530

    added implementation for Diff.java

--- Diff.java
+++ Diff.java
@@ -1,10 +1,7 @@
 public class Diff{
 
     public static void main(String[] args){
-        int a = 5;
-        int b = 6;
-
-        System.out.println("The difference is : " + (a-b));
+        
     }
 
 }
